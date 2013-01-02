b27368c9f11cee78d2ca558f3fe8e6086200b91a
Michał <mmichalf@gmail.com>
2013-01-02 12:47:04 +0100

    dodano klasę RootObject

---
+++ AirPlanesOpenGL/AirPlanesOpenGL/src/RootObject.cpp
@@ -0,0 +1,42 @@
+#include "RootObject.h"
+
+
+RootObject::RootObject(void)
+{
+
+}
+
+RootObject::RootObject(GLuint listID)
+{
+	for(int i=0;i<2;++i)
+		_existence.push_back(new GLfloat[3]);
+
+	for(int i=0;i<_existence.size();++i)
+		for(int j=0;j<3;++j)
+			_existence[i][j] = 0.0f;
+}
+
+void RootObject::setPosition(GLfloat x,GLfloat y,GLfloat z) 
+{ 
+	_existence[0][0] = x;
+	_existence[0][1] = y;
+	_existence[0][2] = z;
+}
+
+void RootObject::setPosition(GLfloat pos[3]) 
+{ 
+	for(int i=0;i<3;++i)
+		_existence[0][i] = pos[i];
+}
+
+void RootObject::addRotate(GLfloat x, GLfloat y, GLfloat z) 
+{
+	_existence[1][0] += x;
+	_existence[1][1] += y;
+	_existence[1][2] += z;
+}
+
+
+RootObject::~RootObject(void)
+{
+}
---
+++ AirPlanesOpenGL/AirPlanesOpenGL/src/RootObject.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <Windows.h>
+#include <gl/GL.h>
+#include <vector>
+
+class RootObject
+{
+public:
+	RootObject(void);
+	RootObject(GLuint listID);
+
+	// lista - ID obiektu
+	inline void setList(GLuint listID) { _listID = listID; };
+	inline GLuint getList() { return _listID; }
+
+	// pozycja
+	void setPosition(GLfloat position[3]);
+	void setPosition(GLfloat x,GLfloat y,GLfloat z);
+	inline GLfloat* getPosition() { return _existence[0];};	
+
+	// obrót obiektu
+	virtual void addRotate(GLfloat x, GLfloat y, GLfloat z);
+	inline GLfloat* getRotate() { return _existence[1] ; }
+
+	// przesuniêcie
+	void move(GLfloat x, GLfloat y, GLfloat z) 
+	{ 
+		_existence[0][0] += x;
+		_existence[0][1] += y;
+		_existence[0][2] += z;
+	}
+	
+	virtual void doSth() = 0;
+	virtual void render() {};
+
+	virtual ~RootObject(void);
+
+	GLuint	_listID;
+
+	//1 elem pos , 2 elem rot 
+	std::vector<GLfloat*> _existence; 
+};
+
